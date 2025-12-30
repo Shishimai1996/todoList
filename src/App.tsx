05901ac9dc47d4fca4aedb5f90bdd56e758edae1
清水舞 <s.amandamy2161@gmail.com>
2025-12-30 12:53:15 +0900

    add nest backend application

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
 import { Todo } from "./todo.model";
@@ -7,14 +7,26 @@ import { CountUp } from "./components/CountUp";
 const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]); //by todo.model.ts
 
-  const todoAddHandler = (text: string) => {
-    setTodos((prevTodos) => [
-      ...prevTodos,
-      { id: Math.random().toString(), text: text },
-    ]); //既存のステートを残して新規入力値を追加。
+  useEffect(() => {
+    fetch("http://localhost:3001/todos")
+      .then((res) => res.json())
+      .then((data) => setTodos(data));
+  }, []);
+
+  const todoAddHandler = async (text: string) => {
+    const res = await fetch("http://localhost:3001/todos", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text }),
+    });
+    const newTodo = await res.json();
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
   };
 
-  const todoDeleteHandler = (todoId: string) => {
+  const todoDeleteHandler = async (todoId: string) => {
+    await fetch(`http://localhost:3001/todos/${todoId}`, {
+      method: "DELETE",
+    });
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== todoId)); //todoIdとtodo.idが一致したら削除。しなかったら新しい配列に格納する。★
   };
   return (
